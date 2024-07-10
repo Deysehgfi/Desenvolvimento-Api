16076711a4cca692cf01a665035975f6ccd08c63
Deyseane Pereira <deyseane2006@gmail.com>
2024-07-10 08:25:39 -0300

    Conceitos sobre request

--- 1_express/server.js
+++ 1_express/server.js
@@ -5,22 +5,68 @@ const PORT = 3333;
 //cria um servidor 
 const app = express()
 
+//Aceitar JSON
+app.use(express.json())
+
+
+
+
+/*Rotas
+Request HTTP
+query params - ...:3333/pessoas?nome="Carlos"&idade=32
+*Rotas do tipo GET (utilizado para crir FILTROS e BUSCAS)
+
+
+route params - ...:3333/pessoas/5
+*Rotas tipo GET, PUT, PATCH, DELETE(listar um elemento)
+
+
+body params - ...:3333/pessoas
+*Rotas do tipo POST (Cadastro de informções)
+*/
+
 //rota com method === get 
 //rota , função de callbak com requisição e a resposta
-app.get("/", (request, response)=>{
-// response.status(200).json({msg: "GET"})
-//cabeçalho 200 retorna como objeto json com uma menssage
+// app.get("/users", (request, response)=>{
+// // response.status(200).json({msg: "GET"})
+// //cabeçalho 200 retorna como objeto json com uma menssage
 
-response.status(200).json([
-    "Pessoas 1",
-    "Pessoas 2",
-    "Pessoas 3"
-]) 
-})
+// response.status(200).json([
+//     "Pessoas 1",
+//     "Pessoas 2",
+//     "Pessoas 3"
+// ]) 
+// })
+
+
+//Trabalhando com as rotas query
+app.get("/users", (request, response)=>{
+//    const query = request.query 
+//    console.log(query)
+
+const {nome , idade} = request.query
+console.log(nome,idade)
+    
+    response.status(200).json([
+        "Pessoas 1",
+        "Pessoas 2",
+        "Pessoas 3"
+    ]) 
+    })
+
 
 //post
-app.post("/users", (request,response)=>{
+app.post("/users", (request, response)=>{
     // response.status(200).json({msg:"POST"})
+
+    // const body = request.body
+    // console.log(body)
+
+
+    const {nome, idade} = request.body
+    console.log(nome, idade)
+
+
     response.status(201).json([
         "Pessoas 1",
         "Pessoas 2",
@@ -30,9 +76,24 @@ app.post("/users", (request,response)=>{
 }) 
 
 
+
+
 //put
-app.put("/users", (request,response)=>{
-    // response.status(200).json({msg:"PUT"})
+//Routes Params 
+//referencia ao id
+app.put("/users/:id/:cpf", (request, response)=>{
+    // const params = request.params
+    // console.log(params)
+
+
+    //nos parametros em uma propriedade id 
+    // const id = request.params.id
+    // const cpf = request.params.cpf
+    // console.log(id , cpf)
+
+    const {id, cpf} = request.params //Desestruturando
+    console.log(id,cpf)
+    // response.status(200).json({msg:"PUT"}) -> estrutura base
     response.status(201).json([
         "Pessoas 1",
         "Pessoas 10",
@@ -42,11 +103,11 @@ app.put("/users", (request,response)=>{
 })
 
 
-app.patch("/users", (request,response)=>{
+app.patch("/users", (request, response)=>{
     response.status(200).json({msg:"PATCH"})
 })
 
-app.delete("/users", (request,response)=>{
+app.delete("/users", (request, response)=>{
     // response.status(200).json({msg:"DELETE"})
     response.status(204).json([
         "Pessoas 1",
@@ -65,3 +126,5 @@ app.listen(PORT, ()=>{
 -Divide o terminal em 2
 
 */
+
+
